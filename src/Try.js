09adc2e762e5491d5d5941b6de8e8fdcbd6269e6
Ayushi Mansingka <ayushimansingka@Ayushis-MacBook-Pro.local>
2017-03-07 19:08:41 +0530

    updated with router

--- src/Try.js
+++ src/Try.js
@@ -12,7 +12,8 @@ class Try extends Component
             fetch('https://api.github.com/search/repositories?q=react')
             .then((response)=>response.json())
             .then((responsejson)=> {
-            this.setState({results:responsejson.items})
+            this.setState({results:responsejson.items[5]})
+          
             })
      }
         
@@ -21,13 +22,11 @@ class Try extends Component
             
             return(
              <ul>
-          {
-            
-            this.state.results.map((item)=> {
-              return <li>{item.name}</li>
-            })
-            
-          }
+
+              <li>ID is {this.state.results.id}</li>
+              <li>Full name is {this.state.results.full_name}</li>
+              <li>Description is {this.state.results.description}</li>
+          
         </ul>
     )
            
