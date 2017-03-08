0545c02077fe4c8823146e36536b56d17053191e
Ayushi Mansingka <ayushimansingka@Ayushis-MacBook-Pro.local>
2017-03-08 12:18:23 +0530

    fetching owner properties

--- src/Try.js
+++ src/Try.js
@@ -6,17 +6,21 @@ class Try extends Component
     {
         super(props);
         this.state={results:[]};
+        this.renderOwner=this.renderOwner.bind(this);
     }
      componentDidMount()
      {
             fetch('https://api.github.com/search/repositories?q=react')
             .then((response)=>response.json())
             .then((responsejson)=> {
-            this.setState({results:responsejson.items[5]})
-          
+            this.setState({results:responsejson.items[1]})
+          console.log(this.state.results.owner.avatar_url);
             })
      }
+    renderOwner()
+    {
         
+    }
     render()
     {
             
@@ -26,7 +30,16 @@ class Try extends Component
               <li>ID is {this.state.results.id}</li>
               <li>Full name is {this.state.results.full_name}</li>
               <li>Description is {this.state.results.description}</li>
-          
+             <ul>
+                 {
+                    (this.state.results.length>0)?
+                        this.state.results.owner.map(function(item) {
+                             return <li>{item.avatar_url}</li>
+                 })
+                 :
+                 <div>Wait for results</div>
+                 }
+             </ul>
         </ul>
     )
            
